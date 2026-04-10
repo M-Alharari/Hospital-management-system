f9a13d0ee0fb6913b29f97e590cfb8cb52300f4e
MOhammed Alharari <mohammedalharari24@gmail.com>
2026-04-10 06:45:47 +0200

    Added Repo, Service, person control and person details form

---
+++ Presentation/People/Controls/ctrlPersonCard.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace Presentation.People
+{
+    partial class ctrlPersonCard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            lblAddress = new Label();
+            label7 = new Label();
+            lblPersonID = new Label();
+            llEditPersonInfo = new LinkLabel();
+            pbPersonImage = new PictureBox();
+            lblCountry = new Label();
+            lblPhone = new Label();
+            lblBirthDate = new Label();
+            label16 = new Label();
+            label17 = new Label();
+            label18 = new Label();
+            lblEmail = new Label();
+            lblGender = new Label();
+            lblNationalNo = new Label();
+            lblFullName = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pbPersonImage).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(lblAddress);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(lblPersonID);
+            groupBox1.Controls.Add(llEditPersonInfo);
+            groupBox1.Controls.Add(pbPersonImage);
+            groupBox1.Controls.Add(lblCountry);
+            groupBox1.Controls.Add(lblPhone);
+            groupBox1.Controls.Add(lblBirthDate);
+            groupBox1.Controls.Add(label16);
+            groupBox1.Controls.Add(label17);
+            groupBox1.Controls.Add(label18);
+            groupBox1.Controls.Add(lblEmail);
+            groupBox1.Controls.Add(lblGender);
+            groupBox1.Controls.Add(lblNationalNo);
+            groupBox1.Controls.Add(lblFullName);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(3, 3);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(671, 330);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Person Information";
+            // 
+            // lblAddress
+            // 
+            lblAddress.AutoSize = true;
+            lblAddress.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblAddress.Location = new Point(122, 289);
+            lblAddress.Name = "lblAddress";
+            lblAddress.Size = new Size(31, 21);
+            lblAddress.TabIndex = 26;
+            lblAddress.Text = "???";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(19, 289);
+            label7.Name = "label7";
+            label7.Size = new Size(69, 21);
+            label7.TabIndex = 25;
+            label7.Text = "Address:";
+            // 
+            // lblPersonID
+            // 
+            lblPersonID.AutoSize = true;
+            lblPersonID.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblPersonID.Location = new Point(122, 47);
+            lblPersonID.Name = "lblPersonID";
+            lblPersonID.Size = new Size(31, 21);
+            lblPersonID.TabIndex = 24;
+            lblPersonID.Text = "???";
+            // 
+            // llEditPersonInfo
+            // 
+            llEditPersonInfo.AutoSize = true;
+            llEditPersonInfo.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            llEditPersonInfo.Location = new Point(537, 89);
+            llEditPersonInfo.Name = "llEditPersonInfo";
+            llEditPersonInfo.Size = new Size(88, 21);
+            llEditPersonInfo.TabIndex = 21;
+            llEditPersonInfo.TabStop = true;
+            llEditPersonInfo.Text = "Edit Picture";
+            llEditPersonInfo.LinkClicked += llEditPersonInfo_LinkClicked;
+            // 
+            // pbPersonImage
+            // 
+            pbPersonImage.Location = new Point(498, 122);
+            pbPersonImage.Name = "pbPersonImage";
+            pbPersonImage.Size = new Size(157, 160);
+            pbPersonImage.TabIndex = 20;
+            pbPersonImage.TabStop = false;
+            // 
+            // lblCountry
+            // 
+            lblCountry.AutoSize = true;
+            lblCountry.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblCountry.Location = new Point(383, 246);
+            lblCountry.Name = "lblCountry";
+            lblCountry.Size = new Size(31, 21);
+            lblCountry.TabIndex = 19;
+            lblCountry.Text = "???";
+            // 
+            // lblPhone
+            // 
+            lblPhone.AutoSize = true;
+            lblPhone.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblPhone.Location = new Point(383, 194);
+            lblPhone.Name = "lblPhone";
+            lblPhone.Size = new Size(31, 21);
+            lblPhone.TabIndex = 18;
+            lblPhone.Text = "???";
+            // 
+            // lblBirthDate
+            // 
+            lblBirthDate.AutoSize = true;
+            lblBirthDate.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblBirthDate.Location = new Point(383, 139);
+            lblBirthDate.Name = "lblBirthDate";
+            lblBirthDate.Size = new Size(31, 21);
+            lblBirthDate.TabIndex = 17;
+            lblBirthDate.Text = "???";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label16.Location = new Point(295, 246);
+            label16.Name = "label16";
+            label16.Size = new Size(69, 21);
+            label16.TabIndex = 14;
+            label16.Text = "Country:";
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label17.Location = new Point(295, 194);
+            label17.Name = "label17";
+            label17.Size = new Size(57, 21);
+            label17.TabIndex = 13;
+            label17.Text = "Phone:";
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label18.Location = new Point(295, 139);
+            label18.Name = "label18";
+            label18.Size = new Size(82, 21);
+            label18.TabIndex = 12;
+            label18.Text = "Birth Date:";
+            // 
+            // lblEmail
+            // 
+            lblEmail.AutoSize = true;
+            lblEmail.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblEmail.Location = new Point(122, 246);
+            lblEmail.Name = "lblEmail";
+            lblEmail.Size = new Size(31, 21);
+            lblEmail.TabIndex = 9;
+            lblEmail.Text = "???";
+            // 
+            // lblGender
+            // 
+            lblGender.AutoSize = true;
+            lblGender.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblGender.Location = new Point(122, 194);
+            lblGender.Name = "lblGender";
+            lblGender.Size = new Size(31, 21);
+            lblGender.TabIndex = 8;
+            lblGender.Text = "???";
+            // 
+            // lblNationalNo
+            // 
+            lblNationalNo.AutoSize = true;
+            lblNationalNo.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblNationalNo.Location = new Point(122, 139);
+            lblNationalNo.Name = "lblNationalNo";
+            lblNationalNo.Size = new Size(31, 21);
+            lblNationalNo.TabIndex = 7;
+            lblNationalNo.Text = "???";
+            // 
+            // lblFullName
+            // 
+            lblFullName.AutoSize = true;
+            lblFullName.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblFullName.ForeColor = Color.Red;
+            lblFullName.Location = new Point(122, 89);
+            lblFullName.Name = "lblFullName";
+            lblFullName.Size = new Size(41, 21);
+            lblFullName.TabIndex = 6;
+            lblFullName.Text = "[???]";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(19, 246);
+            label5.Name = "label5";
+            label5.Size = new Size(51, 21);
+            label5.TabIndex = 4;
+            label5.Text = "Email:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(19, 194);
+            label4.Name = "label4";
+            label4.Size = new Size(64, 21);
+            label4.TabIndex = 3;
+            label4.Text = "Gender:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(19, 139);
+            label3.Name = "label3";
+            label3.Size = new Size(97, 21);
+            label3.TabIndex = 2;
+            label3.Text = "National No:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(19, 89);
+            label2.Name = "label2";
+            label2.Size = new Size(84, 21);
+            label2.TabIndex = 1;
+            label2.Text = "Full Name:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(19, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(79, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Person ID:";
+            // 
+            // ctrlPersonCard
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(groupBox1);
+            Name = "ctrlPersonCard";
+            Size = new Size(677, 336);
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pbPersonImage).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label lblEmail;
+        private Label lblGender;
+        private Label lblNationalNo;
+        private Label lblFullName;
+        
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private LinkLabel llEditPersonInfo;
+        private PictureBox pbPersonImage;
+        private Label lblCountry;
+        private Label lblPhone;
+        private Label lblBirthDate;
+        private Label label16;
+        private Label label17;
+        private Label label18;
+        private Label lblPersonID;
+        private Label lblAddress;
+        private Label label7;
+    }
+}
---
+++ Presentation/People/Controls/ctrlPersonCard.cs
@@ -0,0 +1,115 @@
+﻿using Business.Services;
+using DataAccess.Models;
+using Presentation.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentation.People
+{
+    public partial class ctrlPersonCard : UserControl
+    {
+        private PersonService _service = new PersonService();
+        private readonly CountryService _countryService = new CountryService();
+        public Person SelectedPersonInfo { get; private set; }
+
+        public int PersonID => SelectedPersonInfo?.Id ?? -1;
+
+        public ctrlPersonCard()
+        {
+            InitializeComponent();
+        }
+
+        public void LoadPersonInfo(int PersonID)
+        {
+            SelectedPersonInfo = _service.GetPersonDetails(PersonID);
+            if (SelectedPersonInfo == null)
+            {
+                ResetPersonInfo();
+                MessageBox.Show("No Person with PersonID = " + PersonID.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            _FillPersonInfo();
+        }
+        public void LoadPersonInfo(string NationalNo)
+        {
+            SelectedPersonInfo = _service.GetPersonDetails(NationalNo);
+            if (SelectedPersonInfo == null)
+            {
+                ResetPersonInfo();
+                MessageBox.Show("No Person with PersonID = " + PersonID.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            _FillPersonInfo();
+        }
+        private void _LoadPersonImage()
+        {
+            if (SelectedPersonInfo.Gender == 0)
+                pbPersonImage.Image = Resources.Male_512;
+            else
+                pbPersonImage.Image = Resources.Female_512;
+
+            string ImagePath = SelectedPersonInfo.ImagePath;
+            if (ImagePath != "")
+                if (File.Exists(ImagePath))
+                    pbPersonImage.ImageLocation = ImagePath;
+                else
+                    MessageBox.Show("Could not find this image: = " + ImagePath, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        }
+
+        private void _FillPersonInfo()
+        {
+            llEditPersonInfo.Enabled = true;
+
+            lblPersonID.Text = SelectedPersonInfo.Id.ToString();
+            lblNationalNo.Text = SelectedPersonInfo.NationalNo;
+            lblFullName.Text = SelectedPersonInfo.FullName;
+            lblGender.Text = SelectedPersonInfo.Gender == 0 ? "Male" : "Female";
+            lblEmail.Text = SelectedPersonInfo.Email;
+            lblPhone.Text = SelectedPersonInfo.Phone;
+            lblBirthDate.Text = SelectedPersonInfo.DateOfBirth.ToShortDateString();
+            var country = _countryService.GetCountryByID(
+     SelectedPersonInfo.NationalityCountryID);
+            lblCountry.Text = country.CountryName;
+            lblAddress.Text = SelectedPersonInfo.Address;
+            _LoadPersonImage();
+
+
+
+
+        }
+
+        public void ResetPersonInfo()
+        {
+
+            lblPersonID.Text = "[????]";
+            lblNationalNo.Text = "[????]";
+            lblFullName.Text = "[????]";
+            pbPersonImage.Image = Resources.Male_512;
+            lblGender.Text = "[????]";
+            lblEmail.Text = "[????]";
+            lblPhone.Text = "[????]";
+            lblBirthDate.Text = "[????]";
+            lblCountry.Text = "[????]";
+            lblAddress.Text = "[????]";
+
+
+        }
+
+        private void llEditPersonInfo_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            AddUpdatePerson frm = new AddUpdatePerson(PersonID);
+            frm.ShowDialog();
+
+            //refresh
+            LoadPersonInfo(PersonID);
+        }
+    }
+}
